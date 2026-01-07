734033a164ace519dffce2acb01ed399613b6ed9
prxblms <malcondev.lds@hotmail.com>
2026-01-07 16:24:19 -0300

    new

--- storage/views.py
+++ storage/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import get_user_model
 
 # Models
 from .models import Archive
 
+# Usados no tratamento dos arquivos
+import io, zipfile
+from django.http import HttpResponse, FileResponse
+
+# User model
+User = get_user_model()
+
 # ------- Funções -------
 
-def upload_files(file_list):
+def upload_files(request):
+    file_list = request.FILES.getlist('file_list')
+    file_owner = request.user
+
     if not file_list:
-        print("Não tem nenhum arquivo")
+        messages.error(request, "Nenhum arquivo foi selecionado para upload!")
         return
     
     for file_obj in file_list:
@@ -22,19 +34,40 @@ def upload_files(file_list):
             )
 
             if created:
-                save_file.owner = None
+                save_file.owner_id = file_owner
                 save_file.save()
+                messages.success(request, "Arquivo(s) enviado(s) com sucesso!")
+                return
         
         except Exception as e:
-            print(e)
+            messages.error(request, e)
             return
+
+def download_files(request):
+    file_list = request.POST.getlist('file-checkbox')
+
+    if not file_list:
+        messages.error(request, "Nenhum arquivo foi selecionado!")
+        return
+
+    if len(file_list) > 1:
+        buffer = io.BytesID()
+
+        try:
+            with zipfile.ZipFile(buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+                for file_obj in file_list:
+                    save_file = file_obj.file_name or f"file_{file_obj.id}.ext"
+                    zip_file.write(file_obj.file.path, arcname=save_file)
+
+        except Exception as e:
+            print(e)
 
 # ------- Views -------
 
 def main_storage(request):
     """
-    Pra não confundir, o usei 'archive' para os arquivos que seram listados no frotend,
-    e 'file_list' pra lista de arquivos que vão ser tratador 'upload, delete, download'.
+    Pra não confundir, usei 'archive' para os arquivos listados no frotend
+    e 'file_list' para a lista de arquivos que vão ser tratados como upload, delete, download e outros.
     """
 
     archives = Archive.objects.all()
@@ -43,7 +76,9 @@ def main_storage(request):
         form_type = request.POST.get('form_type')
 
         if form_type == 'upload_form':
-            file_list = request.FILES.getlist('file_list')
-            upload_files(file_list)
+            upload_files(request)
+
+        elif form_type == 'download_form':
+            download_files(request)
 
     return render(request, 'storage/home.html', {'archives': archives})
