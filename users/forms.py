46a86115e0d8c654af91aa9e40823e8805daf8d8
prxblms <malcondev.lds@hotmail.com>
2026-01-13 10:46:30 -0300

    mudanças nas cores e outros

--- users/forms.py
+++ users/forms.py
@@ -4,13 +4,13 @@ class LoginForm(forms.Form):
     username = forms.CharField(
         max_length=200,
         widget=forms.TextInput(attrs={
-            'class': "bg-stone-800 hover:bg-stone-700 rounded-md p-2 shadow-md outline-none",
+            'class': "bg-stone-900 hover:bg-stone-800 rounded-md p-2 shadow-md outline-none",
             'placeholder': "Digite seu nome de usuário..."
         })
     )
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={
-            'class': "bg-stone-800 hover:bg-stone-700 rounded-md p-2 shadow-md outline-none",
+            'class': "bg-stone-900 hover:bg-stone-800 rounded-md p-2 shadow-md outline-none",
             'placeholder': "Digite sua senha de acesso..."
         })
     )
