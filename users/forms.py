8caf6eed0f438494b2b9ba12718e843bbae5c9f3
Xzhyan <malcondev.lds@hotmail.com>
2026-01-27 16:52:28 -0300

    novas mudanças no design

--- users/forms.py
+++ users/forms.py
@@ -5,12 +5,14 @@ class LoginForm(forms.Form):
         max_length=200,
         widget=forms.TextInput(attrs={
             'class': "w-full max-w-sm bg-stone-900 hover:bg-stone-800 rounded-sm p-2 shadow-md outline-none text-sm",
-            'placeholder': "Usuário"
+            'placeholder': "Usuário",
+            "autocomplete": "username",
         })
     )
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={
             'class': "w-full max-w-sm bg-stone-900 hover:bg-stone-800 rounded-md p-2 shadow-md outline-none text-sm",
-            'placeholder': "Senha"
+            'placeholder': "Senha",
+            "autocomplete": "current-password",
         })
     )
